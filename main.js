f3105530fb0a90396c9cd52ae162dc24255a7c2d
Eliezer Olamide <ajikaneliezer@gmail.com>
2025-07-29 17:57:50 +0100

    slight function and style change

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ document.addEventListener("DOMContentLoaded", function(){
     const button = document.getElementById("change-color-btn") 
     let box = document.getElementById("color-box")
     button.addEventListener("click", function(){
-        box.style.backgroundColor = `rgb(${rndInt(0, 255)}, ${rndInt(0, 255)}, ${rndInt(0, 255)})` //making an rgb color with random values
+        box.style.backgroundColor =  getRandomColor()//making an rgb color with random values
     })
 })
 
 // function to generate random numbers (for random rgb values)
 function rndInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getRandomColor(){
+  return `rgb(${rndInt(0, 255)}, ${rndInt(0, 255)}, ${rndInt(0, 255)})`
 }
